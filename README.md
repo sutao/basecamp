29cc29cc817952b6511a5c8d18a02438b72996aa
Tao Su <tao@sengledcanada.com>
2017-05-20 17:22:51 -0700

    Added bt server

--- README.md
+++ README.md
@@ -2,9 +2,15 @@ Basecamp
 --------
 
 ```sh
-sudo apt-get install python-dev libbluetooth-dev
+sudo apt-get install python-dev libbluetooth-dev python-dbus
 sudo pip install virtualenv
 make venv
 source env/bin/activate
 make install
 ```
+
+To prepare the system:
+
+To add Serial Port Profile:
+Change `/etc/systemd/system/dbus-org.bluez.service` and add `-C` after `bluetoothd` and reboot.
+Run `sudo sdptool add SP`.
--- basecamp/bt_server.py
+++ basecamp/bt_server.py
@@ -1,33 +1,61 @@
 import bluetooth as bt
+import subprocess
+import threading
 
-server_sock=bt.BluetoothSocket(bt.RFCOMM)
-server_sock.bind(("",bt.PORT_ANY))
-server_sock.listen(1)
+UUID = "1fab9d30-2a09-4331-b347-b2c4b5bcf302"
+SERVICE_NAME = "Basecamp"
 
-port = server_sock.getsockname()[1]
 
-uuid = "1fab9d30-2a09-4331-b347-b2c4b5bcf302"
+class BasecampBluetoothServer(object):
+    def __init__(self, client_callback, uuid=None, service_name=None):
+        self.server_sock = None
+        self.server_thread = None
+        self.client_threads = []
+        self.client_callback = client_callback
+        self.uuid = UUID if uuid is None else uuid
+        self.service_name = SERVICE_NAME if service_name is None else service_name
 
-bt.advertise_service(server_sock, "Basecamp",
-                     service_id = uuid,
-                     service_classes = [uuid, bt.SERIAL_PORT_CLASS],
-                     profiles = [ bt.SERIAL_PORT_PROFILE ])
-                   
-print("Waiting for connection on RFCOMM channel %d" % port)
+    def start(self):
+        if self.server_sock is not None:
+            return
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+        assert self.client_callback, "Client callback must be set"
+        assert self.uuid, "Server UUID must be set"
+        assert self.service_name, "Service name must be set"
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
+        # Makes the device discoverable
+        subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
 
-print("disconnected")
+        # Create the bluetooth socket
+        server_sock = bt.BluetoothSocket(bt.RFCOMM)
+        server_sock.bind(("", bt.PORT_ANY))
+        server_sock.listen(1)
 
-client_sock.close()
-server_sock.close()
-print("all done")
+        port = server_sock.getsockname()[1]
+
+        # Advertise the service
+        bt.advertise_service(server_sock, self.service_name,
+                             service_id=self.uuid,
+                             service_classes=[self.uuid, bt.SERIAL_PORT_CLASS],
+                             profiles=[bt.SERIAL_PORT_PROFILE])
+
+        self.server_sock = server_sock
+
+        # Create the serve thread
+        self.server_thread = threading.Thread(target=self.serve)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+        print("Waiting for connection on RFCOMM channel %d" % port)
+
+    def serve(self):
+        while True:
+            try:
+                client_sock, client_info = self.server_sock.accept()
+                print("Accepted connection from ", client_info)
+                self.client_callback(client_sock, client_info)
+            except IOError:
+                pass
+            finally:
+                client_sock.close()
+
